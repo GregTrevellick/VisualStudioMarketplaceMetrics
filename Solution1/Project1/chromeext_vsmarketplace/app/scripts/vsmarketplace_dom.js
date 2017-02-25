9c1946bf4a2ec1e7d658880f45c0c4b4d6996fde
GregTrevellick <gtrevellick@googlemail.com>
2017-02-25 22:37:54 +0000

    .

--- Solution1/Project1/chromeext_vsmarketplace/app/scripts/vsmarketplace_dom.js
+++ Solution1/Project1/chromeext_vsmarketplace/app/scripts/vsmarketplace_dom.js
@@ -1,10 +1,6 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
-    //debugger;
-
     if (request.action == "GetTotalBtn") {
-        //$('#ctl00_ContentPlaceHolder1_strFormattedCaseNo').val('BS94092010');
-        //$('#ctl00_ContentPlaceHolder1_strVRM').val('DJC20A');
         console.log("it was pressed");
     }
 
