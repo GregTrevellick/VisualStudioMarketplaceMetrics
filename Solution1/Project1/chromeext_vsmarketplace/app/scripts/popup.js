398b941eca979101ddea00ce07d8263bf73d9ce0
GregTrevellick <gtrevellick@googlemail.com>
2017-02-25 15:19:30 +0000

    .

--- Solution1/Project1/chromeext_vsmarketplace/app/scripts/popup.js
+++ Solution1/Project1/chromeext_vsmarketplace/app/scripts/popup.js
@@ -1,12 +1,27 @@
 $(function () {
+    console.log('here');
 
-    $('#DaTotal').val('123456');
+    var theTot = 999;
+
+    //grab ALL elements from DOM with selector    .install-count
+
+
+    //sum their values into 'theTot'
+    console.log(theTot);
+
+
+
+    //$('#daTotal').val(theTot);
+    $('#daTotal').innerHtml(theTot);
+
+
+
 
     // When button clicked do something
-    $('#GetTotalBtn').click(function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: "GetTotalBtn" });
-        });
-    });
+    //$('#GetTotalBtn').click(function () {
+    //    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //        chrome.tabs.sendMessage(tabs[0].id, { action: "GetTotalBtn" });
+    //    });
+    //});
   
 });
