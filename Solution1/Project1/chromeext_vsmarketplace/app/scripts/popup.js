9274f1f39299e1bc277430bf0b46339630a0d331
GregTrevellick <gtrevellick@googlemail.com>
2017-02-26 12:54:13 +0000

    .

--- Solution1/Project1/chromeext_vsmarketplace/app/scripts/popup.js
+++ Solution1/Project1/chromeext_vsmarketplace/app/scripts/popup.js
@@ -1,4 +1,31 @@
+// Regex-pattern to check URLs against. It matches URLs like: http[s]://[...]stackoverflow.com[...]
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?marketplace.visualstudio\.com/;
+
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
+
+// When the browser-action button is clicked...
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // ...check the URL of the active tab against our pattern and...
+    if (urlRegex.test(tab.url)) {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, { text: 'report_back' }, doStuffWithDom);
+    }
+});
+
+
+
+
+
 $(function () {
+
+    //var bg = chrome.extension.getBackgroundPage();
+    //var appinfo = bg.appinfo;
+
+
+        
 
     console.log('here');
     var theTot = 999;
--- Solution1/Project1/chromeext_vsmarketplace/app/scripts/vsmarketplace_dom.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-
-    if (request.action == "GetTotalBtn") {
-        console.log("it was pressed");
-    }
-
-});
-
-chrome.runtime.sendMessage({ action: "show" });
