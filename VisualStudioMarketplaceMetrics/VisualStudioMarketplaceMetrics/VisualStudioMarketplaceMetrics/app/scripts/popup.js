dec856cd152971c93e691bfef749d456a20bd41e
GregTrevellick <gtrevellick@googlemail.com>
2017-02-27 20:56:09 +0000

    .

--- VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/app/scripts/popup.js
+++ VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/app/scripts/popup.js
@@ -15,30 +15,30 @@ $(function () {
 
         var totalInstallCount = 0;
         var totalReviewCount = 0;
+        var rowOpen = "<tr>";
         var rowClose = "</tr>";
 
         //Create table
-        var detailGridTable = $("<table/>").attr("id", "DetailGridTable");
-        $("#DetailGrid").append(detailGridTable);
+    //    var detailGridTable = $("<table/>").attr("id", "DetailGridTable");
+    //    $("#DetailGrid").append(detailGridTable);
 
         //Header row
-        var hdrRowOpen = "<tr class='hdr'>";
-        var hdrInstallCount = "<td>" + "Installs" + "</td>";
-        var hdrItemTitle = "<td>" + "Title" + "</td>";
-        var hdrReviewCount = "<td>" + "Reviews" + "</td>";
+     //   var hdrRowOpen = "<tr class='hdr'>";
+   //     var hdrInstallCount = "<td>" + "Installs" + "</td>";
+  //      var hdrItemTitle = "<td>" + "Title" + "</td>";
+     //   var hdrReviewCount = "<td>" + "Reviews" + "</td>";
 
-        $("#DetailGridTable").append(hdrRowOpen + hdrInstallCount + hdrItemTitle + hdrReviewCount + rowClose);
+   //     $("#DetailGridTable").append(hdrRowOpen + hdrInstallCount + hdrItemTitle + hdrReviewCount + rowClose);
 
         //Data rows
         for (var i = 0; i < obj.length; i++)
         {
             totalInstallCount += parseInt(obj[i]["InstallCount"]);
             totalReviewCount += parseInt(obj[i]["ReviewCount"]);
-            var rowOpen = "<tr>";
             var colInstallCount = "<td class='nbr'>" +obj[i]["InstallCount"]+ "</td>";
             var colItemTitle = "<td>" + obj[i]["ItemTitle"] + "</td>";//gregt alt text with full description //gregt href to the site for the vsix
             var colReviewCount = "<td class='nbr'>" + obj[i]["ReviewCount"] + "</td>";//gregt split "Average rating: 5.0 (3 ratings)" into 2 .nbr columns
-            $("#DetailGridTable").append(rowOpen + colInstallCount + colItemTitle + colReviewCount + rowClose);
+            $("#DetailGridTableBody").append(rowOpen + colInstallCount + colItemTitle + colReviewCount + rowClose);
         }
 
         //Set the totals
@@ -46,10 +46,15 @@ $(function () {
         document.getElementById('TotalReviewCount').innerHTML = totalReviewCount;
 
         //Footer (totals) row
-        var ftrRowOpen = "<tr class='ftr'>";
-        var ftrInstallCount = "<td class='nbr'>" +totalInstallCount + "</td>";
-        var ftrItemTitle = "<td>" + "</td>";
-        var ftrReviewCount = "<td class='nbr'>" + totalReviewCount + "</td>";
-        $("#DetailGridTable").append(ftrRowOpen + ftrInstallCount + ftrItemTitle + ftrReviewCount + rowClose);
+   //     var ftrRowOpen = "<tr class='ftr'>";
+   //     var ftrInstallCount = "<td class='nbr'>" +totalInstallCount + "</td>";
+   //     var ftrItemTitle = "<td>" + "</td>";
+   //     var ftrReviewCount = "<td class='nbr'>" + totalReviewCount + "</td>";
+     //   $("#DetailGridTable").append(ftrRowOpen + ftrInstallCount + ftrItemTitle + ftrReviewCount + rowClose);
+
+
+        document.getElementById('GridTotalInstallCount').innerHTML = totalInstallCount;
+        document.getElementById('GridTotalReviewCount').innerHTML = totalReviewCount;
+
     }
 });
