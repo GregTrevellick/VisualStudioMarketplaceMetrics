691ea1ae0ca64ada85a56e61c326d92f14ad7ff8
GregTrevellick <gtrevellick@googlemail.com>
2017-02-27 21:00:48 +0000

    .

--- VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/app/scripts/popup.js
+++ VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/VisualStudioMarketplaceMetrics/app/scripts/popup.js
@@ -18,18 +18,6 @@ $(function () {
         var rowOpen = "<tr>";
         var rowClose = "</tr>";
 
-        //Create table
-    //    var detailGridTable = $("<table/>").attr("id", "DetailGridTable");
-    //    $("#DetailGrid").append(detailGridTable);
-
-        //Header row
-     //   var hdrRowOpen = "<tr class='hdr'>";
-   //     var hdrInstallCount = "<td>" + "Installs" + "</td>";
-  //      var hdrItemTitle = "<td>" + "Title" + "</td>";
-     //   var hdrReviewCount = "<td>" + "Reviews" + "</td>";
-
-   //     $("#DetailGridTable").append(hdrRowOpen + hdrInstallCount + hdrItemTitle + hdrReviewCount + rowClose);
-
         //Data rows
         for (var i = 0; i < obj.length; i++)
         {
@@ -44,14 +32,6 @@ $(function () {
         //Set the totals
         document.getElementById('TotalInstallCount').innerHTML = totalInstallCount;
         document.getElementById('TotalReviewCount').innerHTML = totalReviewCount;
-
-        //Footer (totals) row
-   //     var ftrRowOpen = "<tr class='ftr'>";
-   //     var ftrInstallCount = "<td class='nbr'>" +totalInstallCount + "</td>";
-   //     var ftrItemTitle = "<td>" + "</td>";
-   //     var ftrReviewCount = "<td class='nbr'>" + totalReviewCount + "</td>";
-     //   $("#DetailGridTable").append(ftrRowOpen + ftrInstallCount + ftrItemTitle + ftrReviewCount + rowClose);
-
 
         document.getElementById('GridTotalInstallCount').innerHTML = totalInstallCount;
         document.getElementById('GridTotalReviewCount').innerHTML = totalReviewCount;
