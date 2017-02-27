af243da95ed3f504594cfd8bc8cbfa34e95268e3
GregTrevellick <gtrevellick@googlemail.com>
2017-02-27 18:12:54 +0000

    .

--- Solution1/Project1/chromeext_vsmarketplace/app/scripts/contentscript_vsmarketplace.js
+++ Solution1/Project1/chromeext_vsmarketplace/app/scripts/contentscript_vsmarketplace.js
@@ -6,14 +6,21 @@ chrome.runtime.onMessage.addListener(
 
             var theDetailsJsonArray = new Array();
 
-            jQuery('.core-info-cell').each(function () {
+            //jQuery('.core-info-cell').each(function () {
+            //jQuery('.gallery-item-card').each(function () {
+            jQuery("[class^=gallery-item-card]").each(function () {
+               // debugger;
                 var theJsonDetails =
                 {
                     InstallCount: $(this).find('.install-count')[0].innerText,
-                    ItemTitle: $(this).find('.item-title')[0].innerText
+                    ItemTitle: $(this).find('.item-title')[0].innerText,
+                    ReviewCount: $(this).find('.rating')[0].title
                 };
                 theDetailsJsonArray.push(theJsonDetails);
-             });
+            });
+
+
+            //
 
             // Call the specified callback
             sendResponse(theDetailsJsonArray);
--- Solution1/Project1/chromeext_vsmarketplace/app/scripts/popup.js
+++ Solution1/Project1/chromeext_vsmarketplace/app/scripts/popup.js
@@ -6,27 +6,50 @@ $(function () {
         chrome.tabs.query(
             { active: true, currentWindow: true },
             function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { action: "sendTotalsToPopUp" }, doStuffWithDomCallBack);
+                chrome.tabs.sendMessage(tabs[0].id, { action: "sendTotalsToPopUp" }, domCallBack);
             });
     };
 
         
-    function doStuffWithDomCallBack(obj) {
+    function domCallBack(obj) {
 
         var totalInstallCount = 0;
+        var totalReviewCount = 0;
+        var rowClose = "</tr>";
 
-        var tbl = $("<table/>").attr("id", "DetailTable");
-        $("#DetailGrid").append(tbl);
+        //Create table
+        var detailGridTable = $("<table/>").attr("id", "DetailGridTable");
+        $("#DetailGrid").append(detailGridTable);
 
+        //Header row
+        var hdrRowOpen = "<tr class='hdr'>";
+        var hdrInstallCount = "<td>" + "Installs" + "</td>";
+        var hdrItemTitle = "<td>" + "Title" + "</td>";
+        var hdrReviewCount = "<td>" + "Reviews" + "</td>";
+
+        $("#DetailGridTable").append(hdrRowOpen + hdrInstallCount + hdrItemTitle + hdrReviewCount + rowClose);
+
+        //Data rows
         for (var i = 0; i < obj.length; i++)
         {
-            var tr = "<tr>";
-            var td1 = "<td>" + obj[i]["InstallCount"] + "</td>";
             totalInstallCount += parseInt(obj[i]["InstallCount"]);
-            var tdLast = "<td>" + obj[i]["ItemTitle"] + "</td></tr>";
-            $("#DetailTable").append(tr + td1 + tdLast);
+            totalReviewCount += parseInt(obj[i]["ReviewCount"]);
+            var rowOpen = "<tr>";
+            var colInstallCount = "<td class='nbr'>" +obj[i]["InstallCount"]+ "</td>";
+            var colItemTitle = "<td>" + obj[i]["ItemTitle"] + "</td>";//gregt alt text with full description //gregt href to the site for the vsix
+            var colReviewCount = "<td class='nbr'>" + obj[i]["ReviewCount"] + "</td>";//gregt split "Average rating: 5.0 (3 ratings)" into 2 .nbr columns
+            $("#DetailGridTable").append(rowOpen + colInstallCount + colItemTitle + colReviewCount + rowClose);
         }
 
+        //Set the totals
         document.getElementById('TotalInstallCount').innerHTML = totalInstallCount;
+        document.getElementById('TotalReviewCount').innerHTML = totalReviewCount;
+
+        //Footer (totals) row
+        var ftrRowOpen = "<tr class='ftr'>";
+        var ftrInstallCount = "<td class='nbr'>" +totalInstallCount + "</td>";
+        var ftrItemTitle = "<td>" + "</td>";
+        var ftrReviewCount = "<td class='nbr'>" + totalReviewCount + "</td>";
+        $("#DetailGridTable").append(ftrRowOpen + ftrInstallCount + ftrItemTitle + ftrReviewCount + rowClose);
     }
 });
