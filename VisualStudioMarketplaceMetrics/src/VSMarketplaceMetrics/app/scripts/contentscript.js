23348aa36d08f3b137e5ec9dfec3c543a6ddef32
GregTrevellick <gtrevellick@googlemail.com>
2017-03-03 22:35:13 +0000

    .

--- VisualStudioMarketplaceMetrics/src/VSMarketplaceMetrics/app/scripts/contentscript.js
+++ VisualStudioMarketplaceMetrics/src/VSMarketplaceMetrics/app/scripts/contentscript.js
@@ -6,10 +6,6 @@ chrome.runtime.onMessage.addListener(
         if (request.action === "requestDomFromVsmpPopUp") {
 
             //NO
-            //https://marketplace.visualstudio.com/manage
-            //https://marketplace.visualstudio.com/subscriptions
-            //https://marketplace.visualstudio.com/items?itemName=vs-publisher-1455028.OpsHubVisualStudioOnlineMigrationUtility
-            //https://marketplace.visualstudio.com/items?itemName=GregTrevellick.OpeninMarkdownMonster
 
             var typ = "";
             var href = window.location.href;
@@ -46,91 +42,138 @@ chrome.runtime.onMessage.addListener(
                             }
                         }
                     }
-                    break;
+                    else {
+                        if (href.indexOf("marketplace.visualstudio.com/manage") != -1
+                         || href.indexOf("marketplace.visualstudio.com/subscriptions") != -1
+                         || href.indexOf("marketplace.visualstudio.com/items?itemName=") != -1) {
+                            typ = "notAllowed";
+                        }
+                        break;
+                    }
             }
-            
-            var vsmpDomJsonDataArray = new Array();
+                    var vsmpDomJsonDataArray = new Array();
 
-            if (typ == "search_VS" ||
-                typ == "search_VSCode")
-            {
-                $("[class^=grid-item]").each(function () {
-                    var installCountRounded = $(this).find('.install-count')[0].innerText;
-                    var installCount = installCountRounded.replace("M", "000000").replace("K", "000").replace(".", "");//gregt 1.9M is not 19000000
-                    var icon = $(this).find('.item-icon')[0].src;
-                    var itemTitle = $(this).find('.item-title')[0].innerText;
-                    var reviewTitle = $(this).find('.rating')[0].title;
-                    var startReview = reviewTitle.indexOf('(') + 1;
-                    var endReview = reviewTitle.indexOf(' ', startReview);
-                    var reviewCount = reviewTitle.substring(startReview, endReview);
-                    var publisher = $(this).find('.publisher')[0].innerText;
-                    var price = $(this).find('.pricing-tag')[0].innerText;
-                    var averageReviewFull = $(this).find('.rating')[0].title;
-                    var averageReviewSplit = averageReviewFull.replace(" ", "").split(":");
-                    var averageReviewNumberPlus = averageReviewSplit[1].split("(");
-                    var averageReview = averageReviewNumberPlus[0];
-                    var fullDescription = $(this).find('.description')[0].innerText;
-                    var url = $(this).find('.gallery-item-card-container')[0].href;
-                    var vsmpDomJsonData =
-                    {
-                        InstallCount: installCount,
-                        Icon: icon,
-                        ItemTitle: itemTitle,
-                        ReviewCount: reviewCount,
-                        Publisher: publisher,
-                        Price: price,
-                        AverageReview: averageReview,
-                        FullDescription: fullDescription,
-                        URL: url,
-                    };
-                    vsmpDomJsonDataArray.push(vsmpDomJsonData);
-                });
-            }
+                    if (typ == "search_VS" ||
+                        typ == "search_VSCode") {
+                        $("[class^=grid-item]").each(function () {
+                            var installCountRounded = $(this).find('.install-count')[0].innerText;
+                            var installCount = installCountRounded.replace("M", "000000").replace("K", "000").replace(".", "");//gregt 1.9M is not 19000000
+                            var icon = $(this).find('.item-icon')[0].src;
+                            var itemTitle = $(this).find('.item-title')[0].innerText;
+                            var reviewTitle = $(this).find('.rating')[0].title;
+                            var startReview = reviewTitle.indexOf('(') + 1;
+                            var endReview = reviewTitle.indexOf(' ', startReview);
+                            var reviewCount = reviewTitle.substring(startReview, endReview);
+                            var publisher = $(this).find('.publisher')[0].innerText;
+                            var price = $(this).find('.pricing-tag')[0].innerText;
+                            var averageReviewFull = $(this).find('.rating')[0].title;
+                            var averageReviewSplit = averageReviewFull.replace(" ", "").split(":");
+                            var averageReviewNumberPlus = averageReviewSplit[1].split("(");
+                            var averageReview = averageReviewNumberPlus[0];
+                            var fullDescription = $(this).find('.description')[0].innerText;
+                            var url = $(this).find('.gallery-item-card-container')[0].href;
+                            var vsmpDomJsonData =
+                            {
+                                InstallCount: installCount,
+                                Icon: icon,
+                                ItemTitle: itemTitle,
+                                ReviewCount: reviewCount,
+                                Publisher: publisher,
+                                Price: price,
+                                AverageReview: averageReview,
+                                FullDescription: fullDescription,
+                                URL: url,
+                            };
+                            vsmpDomJsonDataArray.push(vsmpDomJsonData);
+                        });
+                    }
 
-            if (typ == "browse_Root" ||
-                typ == "browse_VS" ||
-                typ == "browse_VSTS" ||
-                typ == "browse_VSCode" ||
-                typ == "search_VSTS")
-            {
-                $("[class^=gallery-item-card]").each(function () {
-                    var installCountRounded = $(this).find('.install-count')[0].innerText;
-                    var installCount = installCountRounded.replace("M", "000000").replace("K", "000").replace(".", "");//gregt 1.9M is not 19000000
-                    var icon = $(this).find('.item-icon')[0].src;
-                    var itemTitle = $(this).find('.item-title')[0].innerText;
-                    var reviewTitle = $(this).find('.rating')[0].title;
-                    var startReview = reviewTitle.indexOf('(') + 1;
-                    var endReview = reviewTitle.indexOf(' ', startReview);
-                    var reviewCount = reviewTitle.substring(startReview, endReview);
-                    var publisher = $(this).find('.publisher')[0].innerText;
-                    var price = $(this).find('.pricing-tag')[0].innerText;
-                    var averageReviewFull = $(this).find('.rating')[0].title;
-                    var averageReviewSplit = averageReviewFull.replace(" ", "").split(":");
-                    var averageReviewNumberPlus = averageReviewSplit[1].split("(");
-                    var averageReview = averageReviewNumberPlus[0];
-                    var fullDescription = $(this).find('.icon-cell')[0].title;
-                    var url = $(this).find('.gallery-item-card-container').href;
+                    if (typ == "browse_Root" ||
+                        typ == "browse_VS" ||
+                        typ == "browse_VSTS" ||
+                        typ == "browse_VSCode" ||
+                        typ == "search_VSTS") {
+                        $("[class^=gallery-item-card]").each(function () {
+                            var installCountRounded = $(this).find('.install-count')[0].innerText;
+                            var installCount = installCountRounded.replace("M", "000000").replace("K", "000").replace(".", "");//gregt 1.9M is not 19000000
+                            var icon = $(this).find('.item-icon')[0].src;
+                            var itemTitle = $(this).find('.item-title')[0].innerText;
+                            var reviewTitle = $(this).find('.rating')[0].title;
+                            var startReview = reviewTitle.indexOf('(') + 1;
+                            var endReview = reviewTitle.indexOf(' ', startReview);
+                            var reviewCount = reviewTitle.substring(startReview, endReview);
+                            var publisher = $(this).find('.publisher')[0].innerText;
+                            var price = $(this).find('.pricing-tag')[0].innerText;
+                            var averageReviewFull = $(this).find('.rating')[0].title;
+                            var averageReviewSplit = averageReviewFull.replace(" ", "").split(":");
+                            var averageReviewNumberPlus = averageReviewSplit[1].split("(");
+                            var averageReview = averageReviewNumberPlus[0];
+                            var fullDescription = $(this).find('.icon-cell')[0].title;
+                            var url = $(this).find('.gallery-item-card-container').href;
 
-                    var vsmpDomJsonData =
-                    {
-                        InstallCount: installCount,
-                        Icon: icon,
-                        ItemTitle: itemTitle,
-                        ReviewCount: reviewCount,
-                        Publisher: publisher,
-                        Price: price,
-                        AverageReview: averageReview,
-                        FullDescription: fullDescription,
-                        URL: url,
-                    };
-                    vsmpDomJsonDataArray.push(vsmpDomJsonData);
-                });
-            }          
+                            var vsmpDomJsonData =
+                            {
+                                InstallCount: installCount,
+                                Icon: icon,
+                                ItemTitle: itemTitle,
+                                ReviewCount: reviewCount,
+                                Publisher: publisher,
+                                Price: price,
+                                AverageReview: averageReview,
+                                FullDescription: fullDescription,
+                                URL: url,
+                            };
+                            vsmpDomJsonDataArray.push(vsmpDomJsonData);
+                        });
+                    }
 
-            // Call the specified callback
-            popUpCallBackFn(vsmpDomJsonDataArray);
+                   console.log("na1");
+               //     console.log(typ);
+                //    console.log("na2");
+
+
+
+                    if (typ == "" ||
+                        typ == "notAllowed") {
+
+                        console.log("na");
+
+                        var na = "n/a";
+                        var installCountRounded = na;//$(this).find('.install-count')[0].innerText;
+                        var installCount = na;//installCountRounded.replace("M", "000000").replace("K", "000").replace(".", "");//gregt 1.9M is not 19000000
+                        var icon = na;// $(this).find('.item-icon')[0].src;
+                        var itemTitle = na;//$(this).find('.item-title')[0].innerText;
+                        var reviewTitle = na;//$(this).find('.rating')[0].title;
+                        var startReview = na;//reviewTitle.indexOf('(') + 1;
+                        var endReview = na;//reviewTitle.indexOf(' ', startReview);
+                        var reviewCount = na;// reviewTitle.substring(startReview, endReview);
+                        var publisher = na;// $(this).find('.publisher')[0].innerText;
+                        var price = na;//$(this).find('.pricing-tag')[0].innerText;
+                        var averageReviewFull = na;// $(this).find('.rating')[0].title;
+                        var averageReviewSplit = na;//averageReviewFull.replace(" ", "").split(":");
+                        var averageReviewNumberPlus = na;//averageReviewSplit[1].split("(");
+                        var averageReview = na;//averageReviewNumberPlus[0];
+                        var fullDescription = na;// $(this).find('.description')[0].innerText;
+                        var url = na;//$(this).find('.gallery-item-card-container')[0].href;
+                        var vsmpDomJsonData =
+                            {
+                                InstallCount: installCount,
+                                Icon: icon,
+                                ItemTitle: itemTitle,
+                                ReviewCount: reviewCount,
+                                Publisher: publisher,
+                                Price: price,
+                                AverageReview: averageReview,
+                                FullDescription: fullDescription,
+                                URL: url,
+                            };
+                        vsmpDomJsonDataArray.push(vsmpDomJsonData);
+                    }
+
+                    // Call the specified callback
+                    popUpCallBackFn(vsmpDomJsonDataArray);
+       //     }
         }
-    }
-);
+    });
 
 chrome.runtime.sendMessage({ action: "openPopUp" });
--- VisualStudioMarketplaceMetrics/src/VSMarketplaceMetrics/app/scripts/popup.js
+++ VisualStudioMarketplaceMetrics/src/VSMarketplaceMetrics/app/scripts/popup.js
@@ -38,80 +38,90 @@ $(function () {
         var rowOpen = "<tr>";
         var rowClose = "</tr>";
 
-        for (var i = 0; i < vsmpDom.length; i++)
-        {
-            var numericInstallCount = parseInt(vsmpDom[i]["InstallCount"]);
-            var numericReviewCount = parseInt(vsmpDom[i]["ReviewCount"]);
-            var numericReviewsAsPercentageOfInstalls = (numericReviewCount / numericInstallCount) * 100;//gregt divide by zero ! //gregt dedupe
-
-            totalInstallCount += numericInstallCount;
-            totalReviewCount += numericReviewCount;
-
-            var colInstallCount = "<td class='numeric'>" + numericInstallCount + "</td>";
-
-            var colItemTitle = "<td>"
-                + "<div title=\"" + vsmpDom[i]["FullDescription"] + "\">"
-                + "<a href=\"" + vsmpDom[i]["URL"] + "\" target=\"_blank\">"
-                + "<img src=\"" + vsmpDom[i]["Icon"] + "\" style=\"width: 18%; height: 18%;\">"
-                + "&nbsp;"
-                + vsmpDom[i]["ItemTitle"]
-                + "</a></div></td>";
-
-            var colReviewCount = "<td class='numeric'>" + numericReviewCount + "</td>";
-           
-            var colReviewsAsPercentageOfInstalls = "<td class='numeric'><div title=\""
-               + numericReviewsAsPercentageOfInstalls.toFixed(9) + "\">"
-               + numericReviewsAsPercentageOfInstalls.toFixed(2) + "</div></td>";
-
-            var colPublisher = "<td>"
-                + "<a href=\""
-                + "https://marketplace.visualstudio.com/search?term=publisher%3A%22"
-                + vsmpDom[i]["Publisher"]
-                + "%22&target=VS&sortBy=Relevance"
-                + "\" target=\"_blank\">"
-                + vsmpDom[i]["Publisher"]
-                + "</a></td>";
-            
-            var colPriceLower = vsmpDom[i]["Price"].toLowerCase();
-            var colPrice = "<td>"
-                + colPriceLower.charAt(0).toUpperCase()
-                + colPriceLower.slice(1)
-                + "</td>";
-
-            var colAverageReview = "<td class='numeric'>" + vsmpDom[i]["AverageReview"] + "</td>";
-
-            $("#DetailGridTableBody").append(
-                rowOpen +
-                colInstallCount +
-                colItemTitle +
-                colReviewCount +
-                colReviewsAsPercentageOfInstalls +
-                colPublisher +
-                colPrice +
-                colAverageReview +
-                rowClose);
+        //  if (vsmpDom[0]["URL"] == "n/a"
+        if (vsmpDom["URL"] == "n/a"
+            //&& vsmpDom[0].url == "na"
+            ) {
+            var colItemTitle = "<td>" + "data not avail" + "</td>";
+            var colReviewCount = "<td>" + "data not avail" + "</td>";
         }
+        else {
 
-        var totalReviewsAsPercentageOfTotalInstalls = (totalReviewCount / totalInstallCount) * 100;//gregt divide by zero ! //gregt dedupe
+            for (var i = 0; i < vsmpDom.length; i++) {
+                var numericInstallCount = parseInt(vsmpDom[i]["InstallCount"]);
+                var numericReviewCount = parseInt(vsmpDom[i]["ReviewCount"]);
+                var numericReviewsAsPercentageOfInstalls = (numericReviewCount / numericInstallCount) * 100;//gregt divide by zero ! //gregt dedupe
 
-        //H2
-        document.getElementById('TotalExtensionsCount').innerHTML = vsmpDom.length.toLocaleString() + " extensions";
-        document.getElementById('TotalInstallCount').innerHTML = totalInstallCount.toLocaleString() + " installs";
-        document.getElementById('TotalReviewCount').innerHTML =
-            totalReviewCount.toLocaleString()
-            + " reviews ("
-            + totalReviewsAsPercentageOfTotalInstalls.toFixed(3).toLocaleString()
-            + "% of installs)";
-        document.getElementById('TotalReviewCount').title =
-            + totalReviewCount.toLocaleString()
-            + " divided by "
-            + totalInstallCount.toLocaleString();
-        //QUNIT document.getElementById('OverallAverageReview').innerHTML =  + " weighted average reviews";
+                totalInstallCount += numericInstallCount;
+                totalReviewCount += numericReviewCount;
 
-        //TFOOT
-        document.getElementById('GridTotalInstallCount').innerHTML = totalInstallCount.toLocaleString();
-        document.getElementById('GridTotalReviewCount').innerHTML = totalReviewCount.toLocaleString();
-        //QUNIT document.getElementById('AverageReview').innerHTML = totalReviewCount.toLocaleString();
+                var colInstallCount = "<td class='numeric'>" + numericInstallCount + "</td>";
+
+                var colItemTitle = "<td>"
+                    + "<div title=\"" + vsmpDom[i]["FullDescription"] + "\">"
+                    + "<a href=\"" + vsmpDom[i]["URL"] + "\" target=\"_blank\">"
+                    + "<img src=\"" + vsmpDom[i]["Icon"] + "\" style=\"width: 18%; height: 18%;\">"
+                    + "&nbsp;"
+                    + vsmpDom[i]["ItemTitle"]
+                    + "</a></div></td>";
+
+                var colReviewCount = "<td class='numeric'>" + numericReviewCount + "</td>";
+
+                var colReviewsAsPercentageOfInstalls = "<td class='numeric'><div title=\""
+                   + numericReviewsAsPercentageOfInstalls.toFixed(9) + "\">"
+                   + numericReviewsAsPercentageOfInstalls.toFixed(2) + "</div></td>";
+
+                var colPublisher = "<td>"
+                    + "<a href=\""
+                    + "https://marketplace.visualstudio.com/search?term=publisher%3A%22"
+                    + vsmpDom[i]["Publisher"]
+                    + "%22&target=VS&sortBy=Relevance"
+                    + "\" target=\"_blank\">"
+                    + vsmpDom[i]["Publisher"]
+                    + "</a></td>";
+
+                var colPriceLower = vsmpDom[i]["Price"].toLowerCase();
+                var colPrice = "<td>"
+                    + colPriceLower.charAt(0).toUpperCase()
+                    + colPriceLower.slice(1)
+                    + "</td>";
+
+                var colAverageReview = "<td class='numeric'>" + vsmpDom[i]["AverageReview"] + "</td>";
+
+                $("#DetailGridTableBody").append(
+                    rowOpen +
+                    colInstallCount +
+                    colItemTitle +
+                    colReviewCount +
+                    colReviewsAsPercentageOfInstalls +
+                    colPublisher +
+                    colPrice +
+                    colAverageReview +
+                    rowClose);
+            }
+
+            var totalReviewsAsPercentageOfTotalInstalls = (totalReviewCount / totalInstallCount) * 100;//gregt divide by zero ! //gregt dedupe
+
+            //H2
+            document.getElementById('TotalExtensionsCount').innerHTML = vsmpDom.length.toLocaleString() + " extensions";
+            document.getElementById('TotalInstallCount').innerHTML = totalInstallCount.toLocaleString() + " installs";
+            document.getElementById('TotalReviewCount').innerHTML =
+                totalReviewCount.toLocaleString()
+                + " reviews ("
+                + totalReviewsAsPercentageOfTotalInstalls.toFixed(3).toLocaleString()
+                + "% of installs)";
+            document.getElementById('TotalReviewCount').title =
+                +totalReviewCount.toLocaleString()
+                + " divided by "
+                + totalInstallCount.toLocaleString();
+            //QUNIT document.getElementById('OverallAverageReview').innerHTML =  + " weighted average reviews";
+
+            //TFOOT
+            document.getElementById('GridTotalInstallCount').innerHTML = totalInstallCount.toLocaleString();
+            document.getElementById('GridTotalReviewCount').innerHTML = totalReviewCount.toLocaleString();
+            //QUNIT document.getElementById('AverageReview').innerHTML = totalReviewCount.toLocaleString();
+
+        }
 
         //Enable table sorting
         $(document).ready(function () {
