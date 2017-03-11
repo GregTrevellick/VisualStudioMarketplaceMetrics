97ab39eeb3090502d6e37bb47644b39839ca92a2
GregTrevellick <gtrevellick@googlemail.com>
2017-03-11 23:08:45 +0000

    .

--- src/VSMarketplaceMetrics/app/scripts/popup.js
+++ src/VSMarketplaceMetrics/app/scripts/popup.js
@@ -1,5 +1,32 @@
 $(function () {
 
+
+
+
+
+
+    //GA start
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-93512771-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    //GA end
+
+
+
+
+
+
+
+
+
+
+
     latestError = "Uninitialised.";
 
     console.image("http://i.imgur.com/NfNVskCl.png");
