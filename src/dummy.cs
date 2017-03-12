50a1968df58b3f9d968fb4777febd18836ed3083
GregTrevellick <gtrevellick@googlemail.com>
2017-03-12 01:10:16 +0000

    .

--- src/dummy.cs
+++ src/dummy.cs
@@ -4,7 +4,13 @@
     {
         static void Main()
         {
-            // Foo grasshopper !
+            // Foo grasshopper...
+            // Why define this extn in a wholly unneccssary .Net solution file ?
+            // 1. Leverage the MS Build process, in an easy fashion, to expose unintended present files. And vice versa.
+            // 2. Leverage a great IDE in VS, via a parental sln file, in a joined up fashion.
+            // 3. Leverage VS's git integration to permit focus on product, not admin (but what great admin to be fair).
+            // ...lesson endeth bar.
+
         }
     }
 }
