e6c432076ca49a9273335e6d0903a442588d3b09
GregTrevellick <gtrevellick@googlemail.com>
2017-03-17 20:58:33 +0000

    .

--- src/app/scripts/VsmmController.js
+++ src/app/scripts/VsmmController.js
@@ -4,7 +4,7 @@
         /////////function myCtrl1($scope) {
         ////////$scope.modelProp1 = "pr1";
         function () {
-            this.modelProp1 = "pr232";
+            this.TestingTesting123 = "2 b retrieved from xlation code";
       });
 
 }) ();//closure
