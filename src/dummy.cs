c2af4f0d34534ae6e36796450b1ceec8499cafc2
GregTrevellick <gtrevellick@googlemail.com>
2017-04-13 20:09:26 +0100

    .

--- src/dummy.cs
+++ src/dummy.cs
@@ -6,9 +6,9 @@
         {
             // Foo grasshopper...
             // Why define this extn in a wholly unneccssary .Net solution file ?
-            // 1. Leverage the MS Build process, in an easy fashion, to expose unintended present files. And vice versa.
-            // 2. Leverage a great IDE in VS, via a parental sln file, in a joined up fashion.
-            // 3. Leverage VS's git integration to permit focus on product, not admin (but what great admin to be fair).
+            // 1. Leverage the MS Build process (e.g to expose unintended present files. And vice versa.)
+            // 2. Leverage the great text editor that is VS
+            // 3. Leverage VS's git integration rather than learn git cmd line (or another git gui), a prerequisite of which is forcing other more relevant learning out of my brain
             // ...lesson endeth bar.
         }
     }
