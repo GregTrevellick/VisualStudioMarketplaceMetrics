87aa81ccda49e9d59190d2214f6a183f56827fea
Greg Trevellick <gtrevellick@googlemail.com>
2017-10-24 13:38:16 +0100

    added service ref

---
+++ src/VsmpGateway.cs
@@ -0,0 +1,20 @@
+﻿using Project1.VsIdeExtensionService;
+using System.Collections.Generic;
+
+namespace Project1
+{
+    public class VsmpGateway
+    {
+        static void Main()
+        {
+            var client = new VsIdeServiceClient();
+
+            var vsixIds = new string[] { "OpeninXamarinStudio", "WhackWhackTerminal", "ArduinoIDEforVisualStudio" };
+            var requestContext = new Dictionary<string, string>();
+            var currentVersionsForVsixList = client.GetCurrentVersionsForVsixList(vsixIds, requestContext);
+
+            // Always close the client.
+            client.Close();
+        }
+    }
+}
--- src/dummy.cs
+++ src/dummy.cs
@@ -2,15 +2,15 @@
 {
     class Dummy
     {
-        static void Main()
-        {
-            // Foo grasshopper...
-            // Why define this extn in a wholly unneccssary .Net solution file ?
-            // 1. Leverage the MS Build process (e.g to expose unintended present files. And vice versa.)
-            // 2. Leverage the great text editor that is VS
-            // 3. Leverage VS's git integration rather than learn git cmd line (or another git gui), a prerequisite of which is forcing other more relevant learning out of my brain
-            // ...lesson endeth bar.
-        }
+        //static void Main()
+        //{
+        //    // Foo grasshopper...
+        //    // Why define this extn in a wholly unneccssary .Net solution file ?
+        //    // 1. Leverage the MS Build process (e.g to expose unintended present files. And vice versa.)
+        //    // 2. Leverage the great text editor that is VS
+        //    // 3. Leverage VS's git integration rather than learn git cmd line (or another git gui), a prerequisite of which is forcing other more relevant learning out of my brain
+        //    // ...lesson endeth bar.
+        //}
     }
 }
  
