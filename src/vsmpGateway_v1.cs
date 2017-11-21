11960a408c1555b2b5ac85311bf1ff9d4ec9212f
Greg Trevellick <gtrevellick@googlemail.com>
2017-11-21 13:19:43 +0000

    .

---
+++ src/vsmpGateway_v1.cs
@@ -0,0 +1,80 @@
+﻿//https://marketplace.visualstudio.com/services/vstudio/extension.svc
+//http://visualstudiogallery.msdn.microsoft.com/services/v2011/extension.svc
+
+//using project1.vsideserviceclient;
+//using system.collections.generic;
+
+//namespace project1
+//{
+//    public class vsmpGateway_v1
+//    {
+//        static void main()
+//        {
+//            var client = new vsideserviceclient();
+
+//            var vsixids = new string[]
+//            {
+//                "openinxamarinstudio",
+//                "whackwhackterminal",
+//                "arduinoideforvisualstudio",
+//                "madskristensen.editorenhancements",
+//                "2d5de770-50e9-4dcf-87e9-ea1ed1b43b68",
+//                "vspropack.microsoft.15893cd4-2422-4427-bea8-4e28dcc26346"
+//            };
+
+//            var requestcontext = new dictionary<string, string>();
+//            requestcontext.add("osversion", "10.0.10240.0");//must be supplied
+//            requestcontext.add("lcid", "1033");//must be supplied
+//            requestcontext.add("searchsource", "extensionmanagerupdate");//must be supplied
+//            requestcontext.add("vsversion", "14.0");//not required
+//            requestcontext.add("templatetype", null);//not required
+//            requestcontext.add("subskus", null);//not required
+//            requestcontext.add("skus", null);//not required
+
+//            https://gist.githubusercontent.com/lbargaoanu/8e3e0dd2d771c43e8eaa43eff5963cdd/raw/04a6724c96852e7307c5ab9b87ce3a4bbe37ec01/checkvsextensionsupdates.txt
+//            <requestcontext xmlns:b="http://schemas.microsoft.com/2003/10/serialization/arrays" xmlns:i="http://www.w3.org/2001/xmlschema-instance">
+//              <b:keyvalueofstringstring>
+//                <b:key>lcid</b:key>
+//                <b:value>1033</b:value>
+//              </b:keyvalueofstringstring>
+//              <b:keyvalueofstringstring>
+//                <b:key>searchsource</b:key>
+//                <b:value>extensionmanagerupdate</b:value>
+//              </b:keyvalueofstringstring>
+//              <b:keyvalueofstringstring>
+//                <b:key>templatetype</b:key>
+//                <b:value i:nil="true"/>
+//              </b:keyvalueofstringstring>
+//              <b:keyvalueofstringstring>
+//                <b:key>skus</b:key>
+//                <b:value/>
+//              </b:keyvalueofstringstring>
+//              <b:keyvalueofstringstring>
+//                <b:key>subskus</b:key>
+//                <b:value/>
+//              </b:keyvalueofstringstring>
+//              <b:keyvalueofstringstring>
+//                <b:key>osversion</b:key>
+//                <b:value>10.0.10240.0</b:value>
+//              </b:keyvalueofstringstring>
+//              <b:keyvalueofstringstring>
+//                <b:key>vsversion</b:key>
+//                <b:value>14.0</b:value>
+//              </b:keyvalueofstringstring>
+//            </requestcontext>
+
+//            var currentversionsforvsixlist = client.getcurrentversionsforvsixlist(vsixids, requestcontext);
+
+//            var c = string.empty;
+//            var a = client.searchreleases("trevellick",c, c,null,null,null);
+//            var b = client.searchreleases("openinxamarinstudio", c, c, null, null, null);
+//            var a = client.searchreleases2("trevellick", c, c, null, null, requestcontext);
+//            var b = client.searchreleases2("openinxamarinstudio", c, c, null, null, requestcontext);
+
+//            var rootcategories = client.getrootcategories("en-gb");
+
+//            always close the client
+//            client.close();
+//        }
+//    }
+//}
---
+++ src/vsmpGateway_v2.cs
@@ -0,0 +1,48 @@
+﻿//https://marketplace.visualstudio.com/services/VStudio/Extension.svc
+//http://visualstudiogallery.msdn.microsoft.com/Services/v2011/Extension.svc
+
+//using Project1.VsIdeServiceClient;
+//using System.Collections.Generic;
+
+//namespace Project1
+//{
+//    public class vsmpGateway_v2
+//    {
+//        static void Main()
+//        {
+//            var client = new VsIdeServiceClient.VsIdeServiceClient();
+
+//            var items = Search(client);
+
+//            if (items.Length > 0)
+//            {
+//                var foundItem = items[0];
+
+//                foundItem.Project.Metadata.TryGetValue("VsixVersion", out string vers);
+//                foundItem.Project.Metadata.TryGetValue("VsixId", out string v6id);
+
+//                var downloadCount = foundItem.Files[0].DownloadCount;
+//                var metaData = foundItem.Project.Metadata;
+//                var modifiedDate = foundItem.Project.ModifiedDate;
+//                var title = foundItem.Project.Title;
+//                var version = vers;
+//                var vsixId = v6id;
+//            }
+//        }
+
+//        private static Release[] Search(VsIdeServiceClient.VsIdeServiceClient client)
+//        {
+//            var whereClause = "(Project.MetaData['VsixId'] = '41521019-e4c7-480c-8ea8-fc4a2c6f50aa') AND (Project.Metadata['SupportedVSEditions'] LIKE '%15.0.26430.16,Pro;%')";
+//            var orderClause = "Project.Metadata['Ranking'] desc";
+//            var requestContext = new Dictionary<string, string>()
+//            {
+//                { "LCID", "1033" },
+//                {"SearchSource", "ExtensionManagerQuery"},
+//                {"OSVersion","10.0.15063.0"}
+//            };
+
+//            var result = client.SearchReleases2("ErikEJ.SQLServerCompactSQLiteToolbox", whereClause, orderClause, 0, 1, requestContext);
+//            return result.Releases;
+//        }
+//    }
+//}
