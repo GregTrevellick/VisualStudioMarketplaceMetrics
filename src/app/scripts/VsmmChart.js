3a8907df42531152be8bc40838fd3b8af45f57c7
Greg Trevellick <gtrevellick@googlemail.com>
2017-11-23 13:41:38 +0000

    .

--- src/app/scripts/VsmmChart.js
+++ src/app/scripts/VsmmChart.js
@@ -1,7 +1,68 @@
+//daRow.NoOfInstalls
+//DaRows
+//debugger;
+//console.log("here");
+//console.log(DaRows);
+
+
+var chartData = [];
+var chartDataElement = {};
+chartData.push(chartDataElement);
+
+
+
+
+
+
+
+
 var ctx = document.getElementById("myChart").getContext('2d');
 
+//var myChart = new Chart(ctx, {
+//    type: 'bar',
+//    data: {
+//        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//        datasets: [{
+//            label: '# of Votes',
+//            data: [12, 19, 3, 5, 2, 3],
+//            backgroundColor: [
+//                'rgba(255, 99, 132, 0.2)',
+//                'rgba(54, 162, 235, 0.2)',
+//                'rgba(255, 206, 86, 0.2)',
+//                'rgba(75, 192, 192, 0.2)',
+//                'rgba(153, 102, 255, 0.2)',
+//                'rgba(255, 159, 64, 0.2)'
+//            ],
+//            borderColor: [
+//                'rgba(255,99,132,1)',
+//                'rgba(54, 162, 235, 1)',
+//                'rgba(255, 206, 86, 1)',
+//                'rgba(75, 192, 192, 1)',
+//                'rgba(153, 102, 255, 1)',
+//                'rgba(255, 159, 64, 1)'
+//            ],
+//            borderWidth: 1
+//        }]
+//    },
+//    options: {
+//        scales: {
+//            yAxes: [{
+//                ticks: {
+//                    beginAtZero: true
+//                }
+//            }]
+//        }
+//    }
+//});
+
+
+//http://www.chartjs.org/docs/latest/notes/extensions.html
+//https://github.com/jtblin/angular-chart.js
+//http://jtblin.github.io/angular-chart.js/
+//http://jtblin.github.io/angular-chart.js/#getting_started
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'pie',
     data: {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
@@ -14,26 +75,7 @@ var myChart = new Chart(ctx, {
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            ]
         }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
     }
 });
-    
