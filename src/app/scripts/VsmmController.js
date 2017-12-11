862dc67584db9b51d0d30b1fc2424a1d4185664d
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-11 13:19:06 +0000

    .

--- src/app/scripts/VsmmController.js
+++ src/app/scripts/VsmmController.js
@@ -285,33 +285,16 @@
                 'rgba(255, 159, 64, 0.2)'
             ];
 
-        //////////////////////////////////////////////////var labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-
-        //new Chart(document.getElementById("chartjs-4"), 
-        //    {
-        //        "type": "doughnut",
-        //        "data": {
-        //            "labels": ["Red", "Blue", "Yellow"],
-        //            "datasets": [
-        //                {
-        //                    "label": "My First Dataset",
-        //                    "data": [300, 50, 100],
-        //                    "backgroundColor": [
-        //                        "rgb(255, 99, 132)",
-        //                        "rgb(54, 162, 235)",
-        //                        "rgb(255, 205, 86)"]
-        //                }]
-        //        }
-        //    });
 
         var chartData = [];
         var chartDataElement = {};
         chartData.push(chartDataElement);
         var ctx = document.getElementById("myChart").getContext('2d');
+
         var myChart = new Chart(ctx, {
             type: 'pie',
             data: {
-                //labels: daRowsLabels.slice(5),
+                labels: daRowsLabels,
                 datasets: [{
                     label: '# of Votes',
                     data: daRowsNoOfInstalls,
